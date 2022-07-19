5d64efa4e95772e79d0c2db96aeb9123645b6289
ryurim0109 <dbfla5036@naver.com>
2022-07-19 22:46:36 +0900

    카카오 지도 불르기

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,120 +1,111 @@
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import React, { useState, useRef, useEffect } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import MobileFrame from '../src/components/shared/MobileFrame';
-import theme from '../styles/theme';
-import { gsap } from 'gsap';
-import Script from 'next/script';
-import GlobalStyle from '../styles/GlobalStyle';
-import { Provider } from 'react-redux';
-import store from '../store';
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import React, { useState, useRef, useEffect } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import MobileFrame from "../src/components/shared/MobileFrame";
+import theme from "../styles/theme";
+import { gsap } from "gsap";
+import GlobalStyle from "../styles/GlobalStyle";
+import { Provider } from "react-redux";
+import store from "../store";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	const textRef = useRef<HTMLDivElement>(null);
-	const text2Ref = useRef<HTMLDivElement>(null);
-	const [active, setActive] = useState(true);
-	useEffect(() => {
-		const timeout = setTimeout(() => remove(), 2000);
-		return () => clearTimeout(timeout);
-	}, []);
-	useEffect(() => {
-		const timeout = setTimeout(() => show(), 4000);
-		return () => clearTimeout(timeout);
-	}, []);
+  const textRef = useRef<HTMLDivElement>(null);
+  const text2Ref = useRef<HTMLDivElement>(null);
+  const [active, setActive] = useState(true);
+  useEffect(() => {
+    const timeout = setTimeout(() => remove(), 2000);
+    return () => clearTimeout(timeout);
+  }, []);
+  useEffect(() => {
+    const timeout = setTimeout(() => show(), 4000);
+    return () => clearTimeout(timeout);
+  }, []);
 
-	const remove = () => {
-		const ani = gsap.to(textRef.current, {
-			x: -773,
-			display: 'none',
-			duration: 1,
-			ease: 'strong.inOut',
-			onComplete: () => setActive(false),
-		});
-		return () => {
-			ani.kill();
-		};
-	};
-	const show = () => {
-		const ani2 = gsap.to(text2Ref.current, {
-			x: 863,
-			duration: 1,
-			ease: 'strong.inOut',
-		});
-		return () => {
-			ani2.kill();
-		};
-	};
-	return (
-		<>
-			<Head>
-				<meta charSet='utf-8' />
-				<meta name='description' content='오피스 구할 땐 오싹으로 싹가능!' />
-				<title>OSSACK</title>
-				<Script
-					src='//dapi.kakao.com/v2/maps/sdk.js?appkey=674f01fb27a171cd85fcbe447348c03e&libraries=services,clusterer&autoload=false'
-					strategy='beforeInteractive'
-				/>
-				<Script
-					type='text/javascript'
-					src='%REACT_APP_KAKAO_KEY%&libraries=services,clusterer,drawing'></Script>
-				<Script src='https://developers.kakao.com/sdk/js/kakao.min.js'></Script>
-			</Head>
-			<GlobalStyle />
-			<ThemeProvider theme={theme}>
-				<Provider store={store}>
-					<Wrap>
-						<Background>
-							<TextImg ref={textRef} />
-							{!active && <Text02Img ref={text2Ref} />}
-						</Background>
-						<MobileFrame className='MobileFramePage'>
-							<Component {...pageProps} />
-						</MobileFrame>
-					</Wrap>
-				</Provider>
-			</ThemeProvider>
-		</>
-	);
+  const remove = () => {
+    const ani = gsap.to(textRef.current, {
+      x: -773,
+      display: "none",
+      duration: 1,
+      ease: "strong.inOut",
+      onComplete: () => setActive(false),
+    });
+    return () => {
+      ani.kill();
+    };
+  };
+  const show = () => {
+    const ani2 = gsap.to(text2Ref.current, {
+      x: 863,
+      duration: 1,
+      ease: "strong.inOut",
+    });
+    return () => {
+      ani2.kill();
+    };
+  };
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="오피스 구할 땐 오싹으로 싹가능!" />
+        <title>OSSACK</title>
+      </Head>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <Wrap>
+            <Background>
+              <TextImg ref={textRef} />
+              {!active && <Text02Img ref={text2Ref} />}
+            </Background>
+            <MobileFrame className="MobileFramePage">
+              <Component {...pageProps} />
+            </MobileFrame>
+          </Wrap>
+        </Provider>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
 
 const Wrap = styled.div`
-	width: 100vw;
-	height: 100vh;
-	.MobileFramePage {
-		z-index: 999;
-		background-color: #fff;
-	}
+  width: 100vw;
+  height: 100vh;
+  .MobileFramePage {
+    z-index: 999;
+    background-color: #fff;
+  }
 `;
 const Background = styled.div`
-	width: 100vw;
-	height: 100vh;
-	background-image: url(/assets/bg.jpg);
-	background-size: cover;
-	overflow: hidden;
-	position: fixed;
-	top: 0;
-	right: 0;
-	bottom: 0;
-	left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-image: url(/assets/bg.jpg);
+  background-size: cover;
+  overflow: hidden;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
 `;
 const TextImg = styled.div`
-	width: 683px;
-	height: 373px;
-	background-image: url(/assets/bg02.png);
-	background-size: cover;
-	position: fixed;
-	bottom: 90px;
-	left: 90px;
+  width: 683px;
+  height: 373px;
+  background-image: url(/assets/bg02.png);
+  background-size: cover;
+  position: fixed;
+  bottom: 90px;
+  left: 90px;
 `;
 const Text02Img = styled.div`
-	width: 690px;
-	height: 373px;
-	background-image: url(/assets/bg03.png);
-	background-size: cover;
-	position: fixed;
-	bottom: 90px;
-	left: -773px;
+  width: 690px;
+  height: 373px;
+  background-image: url(/assets/bg03.png);
+  background-size: cover;
+  position: fixed;
+  bottom: 90px;
+  left: -773px;
 `;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,23 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default class MyDocument extends Document {
-	render() {
-		return (
-			<Html>
-				<Head>
-					<link
-						rel='stylesheet'
-						as='style'
-						href='https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard.css'
-					/>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            as="style"
+            href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard.css"
+          />
+          <Script src="https://developers.kakao.com/sdk/js/kakao.min.js"></Script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
